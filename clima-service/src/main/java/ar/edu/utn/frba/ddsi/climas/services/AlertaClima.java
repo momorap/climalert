5a18b8f862b9c86c686073c22b1676121fa19cde
momorap <moarrossi@frba.utn.edu.ar>
2026-07-04 19:28:10 -0300

    consumo envio de mails de java

--- clima-service/src/main/java/ar/edu/utn/frba/ddsi/climas/services/AlertaClima.java
+++ clima-service/src/main/java/ar/edu/utn/frba/ddsi/climas/services/AlertaClima.java
@@ -1,31 +1,54 @@
 package ar.edu.utn.frba.ddsi.climas.services;
 
-import ar.edu.utn.frba.ddsi.climas.config.RestClimaProperties;
 import ar.edu.utn.frba.ddsi.climas.models.entities.Clima;
+import ar.edu.utn.frba.ddsi.climas.models.entities.Mail;
 import ar.edu.utn.frba.ddsi.climas.models.repositories.ClimaRepository;
+import ar.edu.utn.frba.ddsi.climas.models.repositories.MailRepository;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-import java.util.Objects;
+import java.util.List;
 
 @Service
 public class AlertaClima {
-  private final RestTemplate restTemplate;
 
+  private final MailRepository mailRepository;
   private final ClimaRepository climaRepository;
-  public AlertaClima( RestTemplate restTemplate, ClimaRepository climaRepository){
-    this.restTemplate = restTemplate;
+  private final JavaMailSender mailSender;
+
+  public AlertaClima(MailRepository mailRepository, JavaMailSender mailSender, ClimaRepository climaRepository) {
+    this.mailSender = mailSender;
     this.climaRepository = climaRepository;
+    this.mailRepository = mailRepository;
   }
+
   private Clima ultimoClimaAlertado;
-  public Clima verificarUltimoClima(){
+
+  public Clima verificarUltimoClima() {
     Clima lastClima = climaRepository.findLast().orElse(null);
-    if (lastClima!= null && !lastClima.equals(ultimoClimaAlertado) && lastClima.getTemperatura() > 35 && lastClima.getHumedad()>60){
+    if (lastClima != null && !lastClima.equals(ultimoClimaAlertado) && lastClima.getTemperatura() > 35 && lastClima.getHumedad() > 60) {
       this.ultimoClimaAlertado = lastClima;
-      this.notificarAlerta();
+      this.notificarAlerta(lastClima);
     }
     return lastClima;
   }
-  public void notificarAlerta(){
-    // TODO
+
+  public void notificarAlerta(Clima clima) {
+    List<Mail> mails = mailRepository.findAll();
+
+    mails.forEach(mail -> enviar(mail.getCorreo(),clima));
+  }
+
+  public void enviar(String destinatario, Clima clima) {
+    SimpleMailMessage mensaje = new SimpleMailMessage();
+
+    mensaje.setTo(destinatario);
+    mensaje.setSubject("Alerta meteorológica");
+    mensaje.setText(
+        "Se detectó una alerta climática.\n\n" +
+            "Temperatura: " + clima.getTemperatura() + "°C\n" +
+            "Humedad: " + clima.getHumedad() + "%");
+
+    mailSender.send(mensaje);
   }
 }
